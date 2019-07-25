4e0067fced5263419b4463e440c2cac39589d90b
Sérgio Moura <smouracalmon@gmail.com>
2019-07-25 14:36:31 -0300

    Adicionado o bootstrap

--- index.php
+++ index.php
@@ -66,21 +66,39 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<meta http-equiv="X-UA-Compatible" content="ie=edge">
 	<title>Cadastro de Funcionários</title>
-	
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">	
 </head>
 <body>
-	<ul>
-		<?php foreach($funcionarios as $c): ?>
-		<li>
-			<span><?= $c['nome'];  ?></span> : 
-			<span><?= $c['email'];  ?></span>
-		</li>
-		<?php endforeach; ?>
-	</ul>
-	<form action="index.php" method="post">
-		<input <?= (in_array('errNome',$erros)?'style="border:red 1px solid"':''); ?> type="text" name="nome" id="nome" placeholder="Digite o nome">
-		<input  type="email" name="email" id="email" placeholder="Digite o e-mail">
-		<button type="submit">Salvar</button>
-	</form>
+	<div class="container">
+		<div class="row">
+			<ul class="col-sm-12 col-md-4 list-group">
+				<?php foreach($funcionarios as $c): ?>
+				<li class="list-group-item">
+					<span><?= $c['nome'];  ?></span> : 
+					<span><?= $c['email'];  ?></span>
+				</li>
+				<?php endforeach; ?>
+			</ul>
+			<form class="col-sm-12 col-md-8" action="index.php" method="post">
+				
+				<div class="form-group">
+					<label for="nome">Nome</label>
+					<input type="text" class="form-control" id="nome" name="nome" placeholder="Digite o nome do funcionário">
+				</div>
+				
+				<div class="form-group">
+					<label for="email">E-mail</label>
+					<input type="email" class="form-control" id="email" name="email" placeholder="Digite o e-mail do funcionário">
+				</div>
+				
+				<button class="btn btn-primary" type="submit">Salvar</button>
+				
+			</form>
+		</div>
+	</div>
+
+	<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
 </body>
 </html>
