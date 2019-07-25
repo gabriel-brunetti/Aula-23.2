1515ad0f2dc8af55ecf8c0165b98ff1b18daba03
Sérgio Moura <smouracalmon@gmail.com>
2019-07-25 14:08:41 -0300

    Adicionado o título

--- index.php
+++ index.php
@@ -65,7 +65,7 @@
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<meta http-equiv="X-UA-Compatible" content="ie=edge">
-	<title>Document</title>
+	<title>Cadastro de Funcionários</title>
 	
 </head>
 <body>
